927cb1d5059118dfe052d3d892b0211f11310d3f
Pequenia <nayelozu.xv@gmail.com>
2023-04-11 18:34:17 -0700

    modo1

---
+++ login/lib/main.dart
@@ -0,0 +1,207 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(),
+      routes: {
+        '/login': (context) => LoginPage(),
+        '/register': (context) => RegisterPage(),
+      },
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  late String _username;
+  late String _password;
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      print('Username: $_username');
+      print('Password: $_password');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login Page'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Username'),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter a username';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _username = value!;
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter a password';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _password = value!;
+                },
+              ),
+              SizedBox(
+                height: 16.0,
+              ),
+              ElevatedButton(
+                onPressed: _submitForm,
+                child: Text('Login'),
+              ),
+              SizedBox(
+                height: 16.0,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/register');
+                },
+                child: Text('Register'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({Key? key}) : super(key: key);
+
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final _formKey = GlobalKey<FormState>();
+  late String _username;
+  late String _email;
+  late String _password;
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      print('Username: $_username');
+      print('Email: $_email');
+      print('Password: $_password');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Register Page'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Username'),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter a username';
+}
+return null;
+},
+onSaved: (value) {
+_username = value!;
+},
+),
+TextFormField(
+decoration: InputDecoration(labelText: 'Email'),
+keyboardType: TextInputType.emailAddress,
+validator: (value) {
+if (value!.isEmpty) {
+return 'Please enter an email';
+} else if (!value.contains('@')) {
+return 'Please enter a valid email';
+}
+return null;
+},
+onSaved: (value) {
+_email = value!;
+},
+),
+TextFormField(
+decoration: InputDecoration(labelText: 'Password'),
+obscureText: true,
+validator: (value) {
+if (value!.isEmpty) {
+return 'Please enter a password';
+} else if (value.length < 8) {
+return 'Password must be at least 8 characters long';
+}
+return null;
+},
+onSaved: (value) {
+_password = value!;
+},
+),
+SizedBox(
+height: 16.0,
+),
+ElevatedButton(
+onPressed: _submitForm,
+child: Text('Register'),
+),
+SizedBox(
+height: 16.0,
+),
+ElevatedButton(
+onPressed: () {
+Navigator.pop(context);
+},
+child: Text('Back to Login'),
+),
+],
+),
+),
+),
+);
+}
+}
